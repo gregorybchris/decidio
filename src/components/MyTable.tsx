6ac22b42a49563dd97a8d6e7ef14bb1faee0bdae
Chris Gregory <gregorybchris@gmail.com>
2023-01-31 00:54:11 -0800

    Update faker data

--- src/components/MyTable.tsx
+++ src/components/MyTable.tsx
@@ -1,16 +1,14 @@
 import {
-  Column,
   ColumnDef,
   RowData,
-  Table,
   flexRender,
   getCoreRowModel,
   getFilteredRowModel,
   getPaginationRowModel,
   useReactTable,
 } from "@tanstack/react-table";
-import { Person, makeData } from "../lib/mockData";
-import { useCallback, useEffect, useMemo, useReducer, useRef, useState } from "react";
+import { OptionRow, makeData } from "../lib/mockData";
+import { useCallback, useEffect, useMemo, useRef, useState } from "react";
 
 declare module "@tanstack/react-table" {
   interface TableMeta<TData extends RowData> {
@@ -18,19 +16,19 @@ declare module "@tanstack/react-table" {
   }
 }
 
-// Give our default column cell renderer editing superpowers!
-const defaultColumn: Partial<ColumnDef<Person>> = {
+// Ability to edit cells
+const defaultColumn: Partial<ColumnDef<OptionRow>> = {
   cell: ({ getValue, row: { index }, column: { id }, table }) => {
     const initialValue = getValue();
-    // We need to keep and update the state of the cell normally
     const [value, setValue] = useState(initialValue);
 
-    // When the input is blurred, we'll call our table meta's updateData function
+    // Handle cell update
     function onBlur() {
       table.options.meta?.updateData(index, id, value);
+      console.log("Editing (row, col, value)", index, id, value);
     }
 
-    // If the initialValue is changed external, sync it up with our state
+    // If the initialValue is changed externally, sync it up with our state
     useEffect(() => {
       setValue(initialValue);
     }, [initialValue]);
@@ -56,61 +54,33 @@ function useSkipper() {
 }
 
 export default function MyTable() {
-  const columns = useMemo<ColumnDef<Person>[]>(
+  const columns = useMemo<ColumnDef<OptionRow>[]>(
     () => [
       {
-        header: "Name",
-        footer: (props) => props.column.id,
-        columns: [
-          {
-            accessorKey: "firstName",
-            footer: (props) => props.column.id,
-          },
-          {
-            accessorFn: (row) => row.lastName,
-            id: "lastName",
-            header: () => <span>Last Name</span>,
-            footer: (props) => props.column.id,
-          },
-        ],
+        accessorKey: "option",
+        header: () => "Option",
       },
       {
-        header: "Info",
-        footer: (props) => props.column.id,
-        columns: [
-          {
-            accessorKey: "age",
-            header: () => "Age",
-            footer: (props) => props.column.id,
-          },
-          {
-            header: "More Info",
-            columns: [
-              {
-                accessorKey: "visits",
-                header: () => <span>Visits</span>,
-                footer: (props) => props.column.id,
-              },
-              {
-                accessorKey: "status",
-                header: "Status",
-                footer: (props) => props.column.id,
-              },
-              {
-                accessorKey: "progress",
-                header: "Profile Progress",
-                footer: (props) => props.column.id,
-              },
-            ],
-          },
-        ],
+        accessorKey: "weight",
+        header: () => "Weight",
+      },
+      {
+        accessorKey: "scoreA",
+        header: () => "Criterion 1",
+      },
+      {
+        accessorKey: "scoreB",
+        header: () => "Criterion 2",
+      },
+      {
+        accessorKey: "scoreC",
+        header: () => "Criterion 3",
       },
     ],
     []
   );
 
   const [data, setData] = useState(() => makeData(1000));
-  const refreshData = () => setData(() => makeData(1000));
 
   const [autoResetPageIndex, skipAutoResetPageIndex] = useSkipper();
 
@@ -150,71 +120,27 @@ export default function MyTable() {
         <thead>
           {table.getHeaderGroups().map((headerGroup) => (
             <tr key={headerGroup.id}>
-              {headerGroup.headers.map((header) => {
-                return (
-                  <th key={header.id} colSpan={header.colSpan}>
-                    {header.isPlaceholder ? null : (
-                      <div>
-                        {flexRender(header.column.columnDef.header, header.getContext())}
-                        {header.column.getCanFilter() ? (
-                          <div>
-                            <Filter column={header.column} table={table} />
-                          </div>
-                        ) : null}
-                      </div>
-                    )}
-                  </th>
-                );
-              })}
+              {headerGroup.headers.map((header) => (
+                <th className="text-left" key={header.id} colSpan={header.colSpan}>
+                  {header.isPlaceholder ? null : (
+                    <div>{flexRender(header.column.columnDef.header, header.getContext())}</div>
+                  )}
+                </th>
+              ))}
             </tr>
           ))}
         </thead>
         <tbody>
-          {table.getRowModel().rows.map((row) => {
-            return (
-              <tr key={row.id}>
-                {row.getVisibleCells().map((cell) => {
-                  return <td key={cell.id}>{flexRender(cell.column.columnDef.cell, cell.getContext())}</td>;
-                })}
-              </tr>
-            );
-          })}
+          {table.getRowModel().rows.map((row) => (
+            <tr className="border-b" key={row.id}>
+              {row.getVisibleCells().map((cell) => (
+                <td key={cell.id}>{flexRender(cell.column.columnDef.cell, cell.getContext())}</td>
+              ))}
+            </tr>
+          ))}
         </tbody>
       </table>
       <div className="h-2" />
-      {/* <div onClick={() => refreshData()}>Refresh Data</div> */}
     </div>
-  );
-}
-
-function Filter({ column, table }: { column: Column<any, any>; table: Table<any> }) {
-  const firstValue = table.getPreFilteredRowModel().flatRows[0]?.getValue(column.id);
-  const columnFilterValue = column.getFilterValue();
-
-  return typeof firstValue === "number" ? (
-    <div className="flex space-x-2">
-      <input
-        type="number"
-        value={(columnFilterValue as [number, number])?.[0] ?? ""}
-        onChange={(e) => column.setFilterValue((old: [number, number]) => [e.target.value, old?.[1]])}
-        placeholder={`Min`}
-        className="w-24 border shadow rounded"
-      />
-      <input
-        type="number"
-        value={(columnFilterValue as [number, number])?.[1] ?? ""}
-        onChange={(e) => column.setFilterValue((old: [number, number]) => [old?.[0], e.target.value])}
-        placeholder={`Max`}
-        className="w-24 border shadow rounded"
-      />
-    </div>
-  ) : (
-    <input
-      type="text"
-      value={(columnFilterValue ?? "") as string}
-      onChange={(e) => column.setFilterValue(e.target.value)}
-      placeholder={`Search...`}
-      className="w-36 border shadow rounded"
-    />
   );
 }
--- src/lib/mockData.ts
+++ src/lib/mockData.ts
@@ -1,13 +1,11 @@
 import { faker } from "@faker-js/faker";
 
-export type Person = {
-  firstName: string;
-  lastName: string;
-  age: number;
-  visits: number;
-  progress: number;
-  status: "relationship" | "complicated" | "single";
-  subRows?: Person[];
+export type OptionRow = {
+  option: string;
+  weight: number;
+  scoreA: number;
+  scoreB: number;
+  scoreC: number;
 };
 
 const range = (len: number) => {
@@ -18,24 +16,22 @@ const range = (len: number) => {
   return arr;
 };
 
-const newPerson = (): Person => {
+const newOptionRow = (): OptionRow => {
   return {
-    firstName: faker.name.firstName(),
-    lastName: faker.name.lastName(),
-    age: faker.datatype.number(40),
-    visits: faker.datatype.number(1000),
-    progress: faker.datatype.number(100),
-    status: faker.helpers.shuffle<Person["status"]>(["relationship", "complicated", "single"])[0]!,
+    option: faker.name.firstName(),
+    weight: faker.datatype.number({ min: 1, max: 10 }),
+    scoreA: faker.datatype.number({ min: 1, max: 5 }),
+    scoreB: faker.datatype.number({ min: 1, max: 5 }),
+    scoreC: faker.datatype.number({ min: 1, max: 5 }),
   };
 };
 
 export function makeData(...lens: number[]) {
-  const makeDataLevel = (depth = 0): Person[] => {
+  const makeDataLevel = (depth = 0): OptionRow[] => {
     const len = lens[depth]!;
-    return range(len).map((d): Person => {
+    return range(len).map((d): OptionRow => {
       return {
-        ...newPerson(),
-        subRows: lens[depth + 1] ? makeDataLevel(depth + 1) : undefined,
+        ...newOptionRow(),
       };
     });
   };
