5ec51907705d9f78b07206864948b14f40519de7
Chris Gregory <gregorybchris@gmail.com>
2025-01-28 10:27:53 -0800

    Update about

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -3,11 +3,11 @@ export default function About() {
     <div>
       <div className="text-3xl font-bold text-slate-800">about</div>
       <div className="mt-6">
-        We make thousands of small choices every day. And most of the time we actually have the winning option picked
+        We make thousands of small choices every day. And most of the time we probably have the winning option picked
         out before we're fully aware that there's even a choice to be made. But a few times in our lives we're
         confronted with truly difficult decisions, choices with consequences we know will be life-changing. At the end
-        of the day, we choose the path that feels right, but getting to a place where anything feels right can be a long
-        process.
+        of the day, we choose the path that <i>feels</i> right, but getting to a place where anything feels right can be
+        a long process.
       </div>
       <div className="mt-6">
         That's where decidio comes in. To help you get to the end of that long decision making process, decidio helps
